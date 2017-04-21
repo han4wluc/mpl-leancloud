2db169b5fd942f51634cf0fbf121b59cb500462e
han4wluc <han4wluc@gmail.com>
2017-04-21 13:02:29 +0800

    add port

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ console.log('app started')
 
 // // 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
 // // LeanEngine 运行时会分配端口并赋值到该变量。
-// var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000);
+var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000);
 
 app.listen(PORT, function (err) {
   console.log('Node app is running on port:', PORT);
