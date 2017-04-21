6f317175ee7cac0f5d6bf67cb99a43367f8d27da
han4wluc <han4wluc@gmail.com>
2017-04-21 12:58:39 +0800

    app listen

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 var AV = require('leanengine');
 var axios = require('axios');
+var express = require('express');
+
 var axs = axios.create({
   // baseURL: 'http://localhost:8080',
   baseURL: process.env.MPL_HOST_PORT,
@@ -16,6 +18,9 @@ AV.init({
 
 // 如果不希望使用 masterKey 权限，可以将下面一行删除
 AV.Cloud.useMasterKey();
+
+var app = express();
+
 
 AV.Cloud.afterSave('_User', function(request) {
 
@@ -39,14 +44,14 @@ console.log('app started')
 // // LeanEngine 运行时会分配端口并赋值到该变量。
 // var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000);
 
-// app.listen(PORT, function (err) {
-//   console.log('Node app is running on port:', PORT);
+app.listen(PORT, function (err) {
+  console.log('Node app is running on port:', PORT);
 
-//   // 注册全局未捕获异常处理器
-//   process.on('uncaughtException', function(err) {
-//     console.error("Caught exception:", err.stack);
-//   });
-//   process.on('unhandledRejection', function(reason, p) {
-//     console.error("Unhandled Rejection at: Promise ", p, " reason: ", reason.stack);
-//   });
-// });
+  // 注册全局未捕获异常处理器
+  process.on('uncaughtException', function(err) {
+    console.error("Caught exception:", err.stack);
+  });
+  process.on('unhandledRejection', function(reason, p) {
+    console.error("Unhandled Rejection at: Promise ", p, " reason: ", reason.stack);
+  });
+});
